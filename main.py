e2d4cbe510e369d611c4421a9e3c8aae9dd97356
Shashank Shridhar <shashankshridhar1@gmail.com>
2023-04-11 10:44:48 +0530

    slight changes

--- main.py
+++ main.py
@@ -5,30 +5,35 @@ import sys
 class Player:
     def __init__(self,image):
         self.image = image
-        self.rect = self.image.get_rect(topleft = (30,350))
-        self.gravity = 2
+        self.rect = self.image.get_rect(center = (30,350))
+        self.gravity = 20
         self.jump_height = 20
         self.velocity = self.jump_height
         self.pos_x = self.rect.x
         self.pos_y = self.rect.y
+        self.HP = 100
         self.jumping = False
 
     def control(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
-            self.rect.move_ip(4.75,0)
+            self.rect.move_ip(4,0)
         if keys[pygame.K_a]:
             if(self.rect.x > 0 or self.rect.x <=500):
-                self.rect.move_ip(-4.75,0)
+                self.rect.move_ip(-4,0)
         if keys[pygame.K_w]:
             self.jumping = True
         if self.jumping:
             self.rect.y -= self.velocity
             self.velocity -= self.gravity
             if self.velocity == -self.jump_height:
-                self.jumping = False
                 self.velocity = self.jump_height
-        
+                self.jumping = False
+                
+    
+    def run_player_methods(self):
+        self.control()
+
 class GoatDemon:
     def __init__(self,image):
         self.image = image
@@ -42,8 +47,8 @@ class Game:
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,self.SCREEN_HEIGHT))
         pygame.display.set_caption('KnightGame')
         self.CLOCK = pygame.time.Clock()
-        self.demon = GoatDemon(self.get_image('cultist.png',24,24,1,5,None))
-        self.player = Player(self.get_image('knight.png',24,24,0,3,None))
+        self.demon = GoatDemon(self.get_image('assets\cultist.png',24,24,1,5,None))
+        self.player = Player(self.get_image('assets\knight.png',24,24,0,3,None))
         
 
     def get_image(self,sheet,width,height,frame,scale,color):
@@ -55,7 +60,7 @@ class Game:
     def update(self):
         self.CLOCK.tick(60)
         pygame.display.flip()
-        self.player.control()
+        self.player.run_player_methods()
         
         #I will probably need to call the player controls function here.
 
